99e937e79a8d47f5be70bbabcd9f06db9420a2bb
Roux Long <rclong1221@gmail.com>
2022-01-03 12:01:32 -0800

    create movie state and api hooks

--- wannabimdb_web/src/App.js
+++ wannabimdb_web/src/App.js
@@ -1,13 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [movies, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token ',
+      }
+    }).then( response => response.json() )
+    .then( response => setMovie(response) )
+    .catch( error => console.log(error) )
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Rater</h1>
       </header>
       <div className="layout">
-          <div>Movie List</div>
+          <div>
+            { movies.map( movie => { return <h2>{movie}</h2>} )}</div>
           <div>Movie Details</div>
         </div>
     </div>
