dca9694d3bc041de4fbda4e3704a6dd9dc0c8eb1
Roux Long <rclong1221@gmail.com>
2022-01-03 23:48:26 -0800

    register user components

--- wannabimdb_web/src/api-service.js
+++ wannabimdb_web/src/api-service.js
@@ -41,4 +41,14 @@ export class API {
             body: JSON.stringify(body),
         }).then( response => response.json() )
     }
+
+    static registerUser(body) {
+        return fetch(`http://127.0.0.1:8000/api/users/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(body),
+        }).then( response => response.json() )
+    }
 }
--- wannabimdb_web/src/components/auth.js
+++ wannabimdb_web/src/components/auth.js
@@ -6,6 +6,7 @@ import { API } from '../api-service';
 function Auth() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoginView, setLoginView] = useState(true);
 
     const [token, setToken] = useCookies(['TokenContext']);
 
@@ -19,13 +20,21 @@ function Auth() {
             .catch(error => console.log(error))
     }
 
+    const registerClicked = () => {
+        API.registerUser({username, password})
+            .then(() => loginClicked())
+            .catch(error => console.log(error))
+    }
+
     return (
         <div>
+            { isLoginView ? <h1>Login</h1> : <h1>Register</h1> }
             <label htmlFor="username">Title</label><br/>
             <input id="username" type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)}></input><br/>
             <label htmlFor="password">Description</label><br/>
             <input id="password" type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}></input><br/>
-            <button onClick={loginClicked}>Log In</button>
+            { isLoginView ? <button onClick={loginClicked}>Log In</button> : <button onClick={registerClicked}>Register</button> }
+            { isLoginView ? <p onClick={() => setLoginView(false) }>You don't have an account.  Register here!</p> : <p onClick={() => setLoginView(true) }>You already have an account.  Login here!</p> }
         </div>
     )
 }
