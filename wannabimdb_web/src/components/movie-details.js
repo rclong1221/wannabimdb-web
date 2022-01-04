8dc79694a2362c7856ef293d93bbcf7ee3b10c55
Roux Long <rclong1221@gmail.com>
2022-01-03 16:10:19 -0800

    add movie rating display and dependencies

--- wannabimdb_web/src/components/movie-details.js
+++ wannabimdb_web/src/components/movie-details.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+import './movie-details.css';
 
 function MovieDetails(props) {
+    const mov = mov;
+
     return (
         <div>
-            { props.movie ? (
+            { mov ? (
                 <div>
-                    <h1>{props.movie && props.movie.title}</h1>
-                    <p>{props.movie && props.movie.description}</p>
+                    <h1>{mov && mov.title}</h1>
+                    <p>{mov && mov.description}</p>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 0 ? 'orange':''} />
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 1 ? 'orange':''} />
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 2 ? 'orange':''} />
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange':''} />
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange':''} />({mov.total_ratings})
                 </div>
             ) : null}
         </div>    
